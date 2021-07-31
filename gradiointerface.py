046b8001831289f6ea93068ab91546e5ca078ff5
Jeffrey Scruggs <jeff.scruggs23@gmail.com>
2021-07-31 00:53:55 -0400

    First MachineLearning Webapp

---
+++ gradiointerface.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import gradio as gr
+import numpy as np
+from keras.preprocessing import image
+from PIL import Image
+
+def classify2(photo):
+    classmodel = tf.keras.models.load_model('cnnMod2Catdog100')
+    #test_image = image.load_img(gData/dataset/training_set/cats/cat.1.jpg', target_size = (64, 64))
+    test_image = image.img_to_array(photo)
+    test_image = np.expand_dims(test_image, axis = 0)
+    result = classmodel.predict(test_image)
+    if result[0][0] == 1:
+        prediction = 'dog'
+    else:
+        prediction = 'cat'
+    return prediction
+
+
+inputclassify = gr.inputs.Image(source="upload", shape=([64,64]), type="pil",)
+classinter1 = gr.Interface(fn=classify2, inputs=[inputclassify], outputs="text")
+classinter1.launch()
