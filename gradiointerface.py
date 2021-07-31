d95187bcf5dc9696882f8d0d5976da9dbb4648ad
Jeffrey Scruggs <jeff.scruggs23@gmail.com>
2021-07-31 00:56:22 -0400

    2econd MachineLearning Webapp

--- gradiointerface.py
+++ gradiointerface.py
@@ -2,10 +2,9 @@ import tensorflow as tf
 import gradio as gr
 import numpy as np
 from keras.preprocessing import image
-from PIL import Image
 
 def classify2(photo):
-    classmodel = tf.keras.models.load_model('cnnMod2Catdog100')
+    classmodel = tf.keras.models.load_model('cnnMod1Catdog100')
     #test_image = image.load_img(gData/dataset/training_set/cats/cat.1.jpg', target_size = (64, 64))
     test_image = image.img_to_array(photo)
     test_image = np.expand_dims(test_image, axis = 0)
