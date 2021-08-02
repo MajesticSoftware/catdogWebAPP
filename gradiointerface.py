8c4dc54d867cac989700e8118b442a7699e9b481
Jeffrey Scruggs <jeff.scruggs23@gmail.com>
2021-08-02 18:00:46 -0400

    2econd MachineLearning Webapp

--- gradiointerface.py
+++ gradiointerface.py
@@ -5,17 +5,17 @@ from keras.preprocessing import image
 
 def classify2(photo):
     classmodel = tf.keras.models.load_model('cnnMod1Catdog100')
-    #test_image = image.load_img(gData/dataset/training_set/cats/cat.1.jpg', target_size = (64, 64))
     test_image = image.img_to_array(photo)
     test_image = np.expand_dims(test_image, axis = 0)
     result = classmodel.predict(test_image)
     if result[0][0] == 1:
-        prediction = 'dog'
+        prediction = 'This is a picture of a dog!'
     else:
-        prediction = 'cat'
+        prediction = 'This is a picture of a cat!'
     return prediction
 
 
-inputclassify = gr.inputs.Image(source="upload", shape=([64,64]), type="pil",)
+
+inputclassify = gr.inputs.Image(source="upload", shape=([64,64]), type="pil", label = "Upload a image of a Cat or Dog to be classified.")
 classinter1 = gr.Interface(fn=classify2, inputs=[inputclassify], outputs="text")
 classinter1.launch()
